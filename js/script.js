1820122f35c6f74b28f780a07a39152b02740b1b
unknown <arthurguilherme2013@gmail.com>
2023-06-24 10:59:13 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggle(){
+    let sec = document.querySelector("#sec");
+    let nav = document.querySelector("#navigation");
+    
+    sec.classList.toggle('active');
+    nav.classList.toggle('active');
+}
